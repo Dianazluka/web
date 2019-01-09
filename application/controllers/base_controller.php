fd5a52c9b47d88d75159850101d8932d9f243991
Dianazluka <deedee123@mail.ru>
2019-01-09 15:38:37 +0600

    Update base_controller.php

--- application/controllers/base_controller.php
+++ application/controllers/base_controller.php
@@ -3,9 +3,21 @@ class BaseController extends Controller
 {
   public function index()
   {
-      $this->view->display('page', [
-          'title' => 'title',
-          'heading' => 'Heading'
-      ]);
+      $login_library = $this->loader->getLibrary('login');
+      if($login_library->checkLogin())
+      {
+        $user = $login_library->getUser();
+        $this->view->display('pages/base', [
+          'login_flag'  => true,
+          'user'        => $user,
+          'title'       => 'Account',
+          'heading'     => 'Welcome, '.ucfirst($user['login'])
+        ]);
+      }
+      else
+      {
+        $url = $this->loader->getLibrary('url');
+        $url->redirect($this->config->common['base_url'].'login');
+      }
   } 
 }
